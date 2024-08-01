359d266ba4aef9bca6a034bfdadce45ac86b1931
zaidhassan168 <127655587+zaidhassan168@users.noreply.github.com>
2024-08-01 17:59:54 +0500

    shadcn-firebase-auth

---
+++ middleware.ts
@@ -0,0 +1,60 @@
+import { NextRequest, NextResponse } from "next/server";
+import { authMiddleware, redirectToHome, redirectToLogin } from "next-firebase-auth-edge";
+import { clientConfig, serverConfig } from "./config";
+
+const PUBLIC_PATHS = ['/register', '/login'];
+
+export async function middleware(request: NextRequest) {
+  return authMiddleware(request, {
+    loginPath: "/api/login",
+    logoutPath: "/api/logout",
+    apiKey: clientConfig.apiKey,
+    cookieName: serverConfig.cookieName,
+    cookieSignatureKeys: serverConfig.cookieSignatureKeys,
+    cookieSerializeOptions: serverConfig.cookieSerializeOptions,
+    serviceAccount: serverConfig.serviceAccount,
+    handleValidToken: async ({token, decodedToken, customToken}, headers) => {
+      // Authenticated user should not be able to access /login, /register and /reset-password routes
+      if (PUBLIC_PATHS.includes(request.nextUrl.pathname)) {
+        console.log('Request:', request);
+
+        return redirectToHome(request);
+      }
+
+      return NextResponse.next({
+        
+        request: {
+          headers
+        }
+      });
+    },
+    handleInvalidToken: async (reason) => {
+      console.log('Request:', request);
+
+      console.info('Missing or malformed credentials', {reason});
+
+      return redirectToLogin(request, {
+        path: '/login',
+        publicPaths: PUBLIC_PATHS
+      });
+    },
+    handleError: async (error) => {
+      console.error('Unhandled authentication error', {error});
+      console.log('Request:', request);
+
+      return redirectToLogin(request, {
+        path: '/login',
+        publicPaths: PUBLIC_PATHS
+      });
+    }
+  });
+}
+
+export const config = {
+  matcher: [
+    "/",
+    "/((?!_next|api|.*\\.).*)",
+    "/api/login",
+    "/api/logout",
+  ],
+};
