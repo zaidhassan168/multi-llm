4949e330a2bef6dd135936632b791b1a70b105c1
Zaidhassan168 <zaidhassan168@outlook.com>
2024-09-14 23:45:27 +0500

    asd

--- app/api/notify/route.ts
+++ app/api/notify/route.ts
@@ -2,13 +2,24 @@
 import { NextResponse } from 'next/server';
 import * as admin from 'firebase-admin';
 import type { NextRequest } from 'next/server';
+import { ServiceAccount } from 'firebase-admin';
 
 // Initialize Firebase Admin SDK if not already initialized
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT_KEY!);
-
+    const serviceAccount = {
+        type: process.env.FIREBASE_TYPE!,
+        project_id: process.env.FIREBASE_PROJECT_ID!,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID!,
+        private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')!,
+        client_email: process.env.FIREBASE_CLIENT_EMAIL!,
+        client_id: process.env.FIREBASE_CLIENT_ID!,
+        auth_uri: process.env.FIREBASE_AUTH_URI!,
+        token_uri: process.env.FIREBASE_TOKEN_URI!,
+        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL!,
+        client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL!,
+      };
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount as ServiceAccount),
   });
 }
 
