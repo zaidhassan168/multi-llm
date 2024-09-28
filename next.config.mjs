5a497ae95e950b6194285b13052904cd00405cba
Zaidhassan168 <127655587+zaidhassan168@users.noreply.github.com>
2024-09-28 19:34:47 +0500

    asa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+   const nextConfig = withPWA({
+     pwa: {
+       dest: 'public',
+       register: true,
+       skipWaiting: true,
+     },
+   });
+
+   export default nextConfig;
